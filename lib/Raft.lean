6bde0bcadbc78bcec49f7a12365ac45e0070121f
filbert <fillberth@gmail.com>
2025-02-08 15:47:27 +0800

    raft: append entries (wip)

--- lib/Raft.lean
+++ lib/Raft.lean
@@ -1,3 +1,4 @@
+import LeanSts.State
 import LeanSts.BFT.Network
 
 /-
@@ -6,7 +7,8 @@ import LeanSts.BFT.Network
 -/
 section Raft
 
-variable {Address Round Value : Type}
+variable {Address Value : Type}
+variable [dec_addr : DecidableEq Address] [dec_value : DecidableEq Value]
 
 @[reducible] def Term := Nat
 @[reducible] def Index := Nat
@@ -18,13 +20,21 @@ inductive Message
     (prevLogIndex : Index)
     (prevLogTerm : Term)
     -- TODO: think about how to represent entries
-    (entries : List Value)
+    (entries : List (Term × Value))
     (leaderCommit : Index)
+  | AppendEntriesReply
+    (term : Term)
+    (fromId : Address)
+    (success : Bool)
   | RequestVote
     (term : Term)
     (candidateId : Address)
     (lastLogIndex : Index)
     (lastLogTerm : Term)
+  | RequestVoteReply
+    (term : Term)
+    (fromId : Address)
+    (voteGranted : Bool)
 
 structure NodeState :=
   /- persistent state on leaders -/
@@ -33,20 +43,32 @@ structure NodeState :=
   currentTerm : Term
   votedFor : Term → Option Address
   -- TODO: type for `log`?
-  log : Round → Option Value
+  log : List (Term × Value)
 
   /- volatile state on servers -/
   commitIndex : Index
-  lastApplied : Index
+
+  /- ignore `lastApplied` temporarily, because we do not
+     specify callbacks yet to handle applying a command to
+     the state machine. -/
+  -- lastApplied : Index
 
   /- volatile state on leaders -/
   /- (re-initialized after election) -/
-  nextIndex : Address -> Index
-  matchIndex : Address -> Index
+  nextIndex : Address → Index
+  matchIndex : Address → Index
 
 local notation "RaftMessage" => (@Message Address Value)
-local notation "RaftState" => (@NodeState Address Round Value)
+local notation "RaftState" => (@NodeState Address Value)
 local notation "RaftPacket" => (Packet Address RaftMessage)
+
+def makePacket (state : RaftState) (dst : Address) (msg: RaftMessage) : RaftPacket :=
+  {
+    src := state.id
+    dst
+    msg
+    consumed := false
+  }
 
 def initLocalState
   (id : Address)
@@ -57,33 +79,106 @@ def initLocalState
     allNodes := nodes
     currentTerm := 0
     votedFor := λ _ ↦ none
-    log := λ _ ↦ none
+    log := []
 
     commitIndex := 0
-    lastApplied := 0
+    -- lastApplied := 0
 
     nextIndex := λ _ ↦ 1
     matchIndex := λ _ ↦ 0
   }
 
--- receiver implementation
+-- follower (receiver) implementation
 def handleAppendEntries
+    (state : RaftState)
     (term : Term)
     (leaderId : Address)
     (prevLogIndex : Index)
     (prevLogTerm : Term)
-    (entries : List Value)
+    -- assume entries are sorted in order of ascending index
+    (entries : List (Term × Value))
     (leaderCommit : Index)
-    : Term × Bool :=
-    sorry
+    : RaftState × List RaftPacket :=
+    let reply (success : Bool) : List RaftPacket :=
+      [makePacket state leaderId (Message.AppendEntriesReply term state.id success)]
+    if term < state.currentTerm then
+      (state, reply false)
+    else
+      match List.get? state.log prevLogIndex with
+      | none =>
+        -- section 5.1
+        (state, reply false)
+      | some (entryTerm, _) =>
+        -- section 5.3
+        if entryTerm = prevLogTerm then
+          let newLog := List.take prevLogIndex state.log
+          let newState := {state with log := newLog}
+          (newState, reply false)
+        else
+          let baseLog := List.take (prevLogIndex + 1) state.log
+          let newLog := List.append baseLog entries
+          let newCommitIndex := max (state.commitIndex) (min leaderCommit (List.length newLog - 1))
+          let newState := {
+            state with
+            log := newLog,
+            commitIndex := newCommitIndex
+          }
+          (newState, reply true)
 
--- receiver implementation
+def handleAppendEntriesReply
+    (state : RaftState)
+    (term : Term)
+    (fromId : Address)
+    (success : Bool)
+    : RaftState × List RaftPacket :=
+    match success with
+    | true =>
+      -- TODO: update matchIndex and nextIndex for follower?
+      sorry
+    | false =>
+      let decrementedNextIndex := (state.nextIndex fromId) - 1
+      let newState := { state with nextIndex := state.nextIndex[fromId ↦ decrementedNextIndex]}
+      let prevLogIndex := decrementedNextIndex - 1
+      let logLength := List.length state.log
+      let ltProp := prevLogIndex < logLength
+      if ltProof : ltProp then
+        let prevLogFin : Fin (List.length state.log) := Fin.mk prevLogIndex ltProof
+        let retryMsg :=
+          Message.AppendEntries
+          state.currentTerm
+          state.id
+          prevLogIndex
+          (Prod.fst (List.get state.log prevLogFin))
+          (List.drop (prevLogIndex + 1) state.log)
+          state.commitIndex
+        (newState, [makePacket state fromId retryMsg])
+      else
+        -- error: prevLogIndex should always be < logLength
+        sorry
+
+-- follower (receiver) implementation
 def handleRequestVote
+    (state : RaftState)
     (term : Term)
     (candidateId : Address)
     (lastLogIndex : Index)
     (lastLogTerm : Term)
-    : Term × Bool :=
+    -- : Term × Bool :=
+    : RaftState × List RaftPacket :=
     sorry
+
+def handleMessage
+  (state : RaftState)
+  (msg : RaftMessage)
+  : RaftState × List RaftPacket :=
+  match msg with
+  | Message.AppendEntries term leaderId prevLogIndex prevLogTerm entries leaderCommit =>
+    handleAppendEntries state term leaderId prevLogIndex prevLogTerm entries leaderCommit
+  | Message.AppendEntriesReply term fromId success =>
+    handleAppendEntriesReply state term fromId success
+  | Message.RequestVote term candidateId lastLogIndex lastLogTerm =>
+    handleRequestVote state term candidateId lastLogIndex lastLogTerm
+  -- todo: handle request vote replies
+  | _ => sorry
 
 end Raft
